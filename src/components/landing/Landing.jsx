f4a9d8d197ca392326483f60cbf13480be1de40e
Eggsdoo <usama.uyk@hotmail.co.uk>
2024-08-18 17:39:30 -0400

    init

---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './landing.css';
+import Main from '../main/Main';
+
+const Landing = () => {
+    return (
+        <>
+        <Main/>
+        </>
+    );
+}
+
+export default Landing;
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './main.css';
+
+const Main = () => {
+    return (
+        <section className="main-section">
+            test
+        </section>
+    );
+}
+
+export default Main;
